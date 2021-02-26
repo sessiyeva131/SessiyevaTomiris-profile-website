98532116353f631a8f7b43477aef718934877910
Tommy <75783755+sessiyeva@users.noreply.github.com>
2021-02-26 22:35:49 +0600

    Add new routes ('/post/create', '/post')

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+
+use App\Models\Post;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +12,15 @@ Route::get('/', function () {
 Route::view('/Portfolio', 'index')->name('portfolio');
 Route::view('/about', 'about')->name('about');
 Route::view('/contact', 'contact')->name('contact');
+
+Route::get('/post/create', function(){
+    DB::table('post')->insert([
+        'title' => 'myDb',
+        'body' => 'Successfully connected!'
+    ]);
+});
+
+Route::get('/post', function(){
+    $mypost = Post::find(1);
+    return "<h2>ID: $mypost->id <br>Title: $mypost->title <br>Body: $mypost->body </h2>";
+});
