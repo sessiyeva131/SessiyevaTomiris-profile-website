5c5ff70935bf3ed204731743062725c5612755dc
Tommy <75783755+sessiyeva@users.noreply.github.com>
2021-03-11 11:57:54 +0600

    Update

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -11,14 +11,13 @@ class BlogController extends Controller
     public function index(){
         $posts = Post::all();
 
-        return view('blog.index')->with(['posts' => $posts]);
+        return view('post.index')->with(['posts' => $posts]);
     }
 
     public function store(Request $request){
         Post::create([
             'title' => $request->title,
             'body' => $request->body
-            
         ]);
 
         return back();
