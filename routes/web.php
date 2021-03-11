ab2552ca58531b5aa9977b246cb9f8489b295ac7
Tommy <75783755+sessiyeva@users.noreply.github.com>
2021-03-11 11:46:26 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
 
@@ -13,10 +14,10 @@ Route::view('/Portfolio', 'index')->name('portfolio');
 Route::view('/about', 'about')->name('about');
 Route::view('/contact', 'contact')->name('contact');
 
-Route::get('/post/create', function(){
+Route::get('/post/add/{title}/{body}', function(){
     DB::table('post')->insert([
-        'title' => 'myDb',
-        'body' => 'Successfully connected!'
+        'title' => '$title',
+        'body' => '$body'
     ]);
 });
 
@@ -24,3 +25,11 @@ Route::get('/post', function(){
     $mypost = Post::find(1);
     return "<h2>ID: $mypost->id <br>Title: $mypost->title <br>Body: $mypost->body </h2>";
 });
+
+Route::get('/blog/index', [BlogController::class, 'index']);
+
+Route::get('/blog/create', function(){
+    return view('post.create');
+});
+
+Route::post('/blog/create', [BlogController::class, 'store'])->name('add_post');
