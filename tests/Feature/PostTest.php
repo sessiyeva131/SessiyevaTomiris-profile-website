39c97ff7d645c8aad5af212d0ed6b9dd2a4e7d30
Tommy <75783755+sessiyeva@users.noreply.github.com>
2021-03-14 21:56:51 +0600

    Create Post.test

---
+++ tests/Feature/PostTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PostTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $response = $this->get('/');
+        $response->assertStatus(200);
+    }
+
+    public function test_get_post_by_id()
+    {
+        $response = $this->get('/post/1');
+        $response->assertStatus(200);
+    }
+
+    public function test_get_all_posts()
+    {
+        $response = $this->get('/blog/index');
+        $response->assertStatus(200);
+    }
+
+    public function test_get_view()
+    {
+        $response = $this->get('/post/2');
+        $response->assertViewHas('info');
+    }
+}
