e32718b3bd48da0c493f5ee7794607b20348ed4b
Tommy <75783755+sessiyeva@users.noreply.github.com>
2021-04-07 03:35:52 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,39 @@
 <?php
 
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\LangController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
 
 use App\Models\Post;
+use App\Models\Users;
+use Illuminate\Support\Facades\App;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::view('/Portfolio', 'index')->name('portfolio');
+// Route::view('/Portfolio', 'index')->name('portfolio');
+Route::get('/profile/{lang}', function($lang){
+    App::setlocale($lang);
+    return view('index');
+})->name('profile');
+
+Route::get('/profile', function(){
+    return view('index');
+})->name('portfolio');
+
+Route::get('/about/{lang}', function($lang){
+    App::setlocale($lang);
+    return view('about');
+})->name('aboutt');
+
+Route::get('/contactt/{lang}', function($lang){
+    App::setlocale($lang);
+    return view('contact');
+})->name('contactt');
+// Route::get('locale/{locale}', 'LangController@change')->name('locale');
+
 Route::view('/about', 'about')->name('about');
 Route::view('/contact', 'contact')->name('contact');
 
@@ -32,5 +55,4 @@ Route::get('/blog/create', function(){
 });
 
 Route::post('/blog/create', [BlogController::class, 'store'])->name('add_post');
-
 Route::get('/post/{id}', [BlogController::class, 'get_info']);
